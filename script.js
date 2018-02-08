bc7cfd3cfd5b270ed040cb9d94dd65c4d0e0fe51
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-02-08 09:40:57 -0500

    tidied js, added reset

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(function () {
+    //                                                   VARIABLES
     var $startButton = $('.startButton');
     var $playerInfo = $('.playerInfo');
     var $grid = $('content');
-    var $resetButton = $('button');
+    var $resetButton = $('.reset');
+    var $cardsFront = $('.front');
+    var $cardsBack = $('.back');
     var $cell = $('.cell');
     var $cell1 = $('.cell1');
     var $cell2 = $('.cell2');
@@ -20,19 +23,13 @@ $(function () {
     var $cell14 = $('.cell14');
     var $cell15 = $('.cell15');
     var $cell16 = $('.cell16');
-    var cellArray = [$cell1, $cell2, $cell3, $cell4, $cell5, $cell6, $cell7, $cell8, 
-                    $cell9, $cell10, $cell11, $cell12, $cell13, $cell14, $cell15, $cell16]
+    var cellArray = [$cell1, $cell2, $cell3, $cell4, $cell5, $cell6, $cell7, $cell8,
+        $cell9, $cell10, $cell11, $cell12, $cell13, $cell14, $cell15, $cell16
+    ]
     var cardColors = ['red', 'red', 'blue', 'blue', 'orange', 'orange', 'yellow', 'yellow',
         'green', 'green', 'purple', 'purple', 'cyan', 'cyan', 'grey', 'grey'
     ]
-       
-    function setColors() {
-        shuffle(cardColors);
-        for (var i = 0; i <= cellArray.length; i++) {
-            cellArray[i].children('.back').addClass(cardColors[i]);
-        }
-
-    }
+    //                                                   FUNCTIONS
     function shuffle(array) {
         var counter = array.length;
         while (counter > 0) {
@@ -45,28 +42,52 @@ $(function () {
         }
         return array;
     }
-    // on load/reset: assign random color classes to each cell 
-    // click start: show player info, content 
-    $grid.hide();
-    $playerInfo.hide();
-    $('.back').hide();
-  
 
-    $startButton.click(function() {
-        
+
+    function setColors(colors, array) {
+
+        for (var i = 0; i <= array.length; i++) {
+
+            array[i].children('.back').attr('id', colors[i]);
+        }
+
+
+    }
+
+    //                                                   EVENT FUNCTIONS
+    $startButton.click(function () {
+
         $playerInfo.slideDown(200);
         $grid.slideDown(200);
         $startButton.hide();
-        setColors(cellArray);
+        shuffle(cardColors);
+        setColors(cardColors, cellArray);
+
     })
-    $('.front').click(function() {
+    $resetButton.click(function () {
+        $grid.hide();
+        $playerInfo.hide();
+        $cardsFront.show();
+        $cardsBack.hide();
+        $startButton.slideDown(800);
+        
+    })
+
+    $cardsFront.click(function () {
         $(this).hide();
         $(this).siblings('.back').show();
-  
-
-        
-
     })
+
+
+    //                                                   WORKFLOW
+    // on load/reset: 
+    $grid.hide();
+    $playerInfo.hide();
+    $cardsBack.hide();
+
+    //assign random color classes to each cell 
+    // click start: show player info, content 
+
     // toggle between player 1 / player 2
     // click cell: hide back color, show front color
     // when 2 cells are shown:
@@ -74,4 +95,3 @@ $(function () {
     // if cell colors match, dissappear and give player 1 or 2 a point 
     // player wins at 5 points 
 })
-
