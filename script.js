cf4521022563f61f35393e273b00a0538212ebc3
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-02-08 12:53:01 -0500

    hello

--- script.js
+++ script.js
@@ -29,6 +29,11 @@ $(function () {
     var cardColors = ['red', 'red', 'blue', 'blue', 'orange', 'orange', 'yellow', 'yellow',
         'green', 'green', 'purple', 'purple', 'cyan', 'cyan', 'grey', 'grey'
     ]
+    var cardsFlipped = 0;
+    var colorArray = [];
+    var cardArray = [];
+    var tryCounter = 0;
+    var thisCard;
     //                                                   FUNCTIONS
     function shuffle(array) {
         var counter = array.length;
@@ -45,13 +50,10 @@ $(function () {
 
 
     function setColors(colors, array) {
-
-        for (var i = 0; i <= array.length; i++) {
-
+        for (var i = 0; i < array.length; i++) {
             array[i].children('.back').attr('id', colors[i]);
+
         }
-
-
     }
 
     //                                                   EVENT FUNCTIONS
@@ -70,13 +72,71 @@ $(function () {
         $cardsFront.show();
         $cardsBack.hide();
         $startButton.slideDown(800);
-        
+
     })
 
     $cardsFront.click(function () {
-        $(this).hide();
-        $(this).siblings('.back').show();
+
+        thisCard = $(this);
+        thisCard.hide();
+        thisCard.siblings('.back').show();
+        cardsFlipped++;
+
+        colorArray.push(thisCard.siblings('.back').attr('id'));
+        cardArray.push(thisCard);
+        cardChecker();
+
+
+
     })
+
+    function cardChecker() {
+
+        if (cardsFlipped === 2) {
+            
+
+            if (colorArray[0] !== colorArray[1]) {
+                cardArray[0].show();
+                cardArray[1].show();
+                $('#' + colorArray[0]).hide();
+                $('#' + colorArray[1]).hide();
+
+            } else {
+                $('#' + colorArray[0]).hide();
+                
+            }
+            tryCounter++;
+            cardsFlipped = 0;
+            cardArray = [];
+            colorArray = [];
+            console.log(cardsFlipped, cardArray, colorArray)
+        }
+    }
+
+
+    //         if (colorArray[0] === colorArray[1]) {
+    //             var thisColor = colorArray[0];
+    //             $('#' + thisColor).hide();
+    //         } else {
+    //             var front1 = cardArray[0];
+    //             var front2 = cardArray[1];
+    //             var back1 = colorArray[0];
+    //             var back2 = colorArray[1];
+    //             front1.show();
+    //             front2.show();
+    //             $('#' + back1).hide();
+    //             $('#' + back2).hide();
+    //             console.log(front1, front2)
+    //         }
+    //     }
+    //     tryCounter ++;
+    //     cardsFlipped = 0;
+    //     cardArray = [];
+    //     colorArray = [];
+
+
+
+
 
 
     //                                                   WORKFLOW
@@ -84,6 +144,22 @@ $(function () {
     $grid.hide();
     $playerInfo.hide();
     $cardsBack.hide();
+
+
+    // while (cardArray.length < 2) {
+    //     console.log(cardArray);
+    //     if (colorArray[0] === colorArray[1]) {
+    //         cardArray[0].slideUp(300);
+    //         cardArray[1].slideUp(300)
+    //         cardsFlipped = 0;
+    //         colorArray = [];
+    //         cardArray = [];
+
+    //     }
+
+
+
+
 
     //assign random color classes to each cell 
     // click start: show player info, content 
@@ -94,4 +170,4 @@ $(function () {
     // if cell colors don't match, flip back over
     // if cell colors match, dissappear and give player 1 or 2 a point 
     // player wins at 5 points 
-})
+});
